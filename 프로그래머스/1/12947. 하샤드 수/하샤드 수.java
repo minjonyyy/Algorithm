91a76c57b29d0e21f594a1e469b4f597e8b5cc1a
minjung <123656628+minjonyyy@users.noreply.github.com>
2025-01-08 20:59:45 +0900

    Update 하샤드 수.java

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,15 +1,9 @@
 class Solution {
     public boolean solution(int x) {
         boolean answer = true;
-        int a = x/10000;
-        int b = (x/1000)%10;
-        int c = (x/100)%10;
-        int d = (x/10)%10;
-        int e = x%10;
+        int a = x / 10000 + (x/1000)%10 + (x/100)%10 + (x/10)%10 + x%10;
         
-        int sum = a+b+c+d+e;
-        
-        if (x%sum == 0){
+        if (x%a == 0){
             answer = true;
         } else {
             answer = false;
