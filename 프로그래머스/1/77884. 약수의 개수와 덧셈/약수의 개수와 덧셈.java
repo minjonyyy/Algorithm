bd137d3615013fe49d4e8a3b7875a4ad0ff25f08
minjung <123656628+minjonyyy@users.noreply.github.com>
2025-02-24 10:07:58 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.86 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -6,8 +6,8 @@ class Solution {
             int cnt = 0;
             for(int j=1; j*j<=i; j++){
                 
-                if(j*j == i) cnt++;
-                else if(i%j==0) cnt += 2;
+                if(j*j == i) { cnt++; }
+                else if(i%j==0) { cnt += 2; }
                 
             }
             
